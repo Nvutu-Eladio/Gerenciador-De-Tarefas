5042b4040153d2a4a00b2bb9de16d1f662c18dc7
Nvutu-Eladio <Eladio2019@hotmail.com>
2024-11-02 23:13:20 -0300

    feat: tabela users criada no banco de dados

---
+++ src/test/java/com/nveladio/com/ao/Gerenciador/De/Tarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nveladio.com.ao.Gerenciador.De.Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
